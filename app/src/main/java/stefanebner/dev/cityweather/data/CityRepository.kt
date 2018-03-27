3ee18ed0b44a26dde8d3de0a9a1a0b9444ac4baf
Stefan Ebner <stefan.ebner.dev@gmail.com>
2018-03-27 15:57:09 +0200

    using rxjava to parse local list of cities

--- app/src/main/java/stefanebner/dev/cityweather/data/CityRepository.kt
+++ app/src/main/java/stefanebner/dev/cityweather/data/CityRepository.kt
@@ -1,8 +1,7 @@
 package stefanebner.dev.cityweather.data
 
-import android.arch.lifecycle.Observer
-import org.jetbrains.anko.doAsync
-import stefanebner.dev.cityweather.data.database.CityDao
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
 import stefanebner.dev.cityweather.data.database.CityDatabase
 import stefanebner.dev.cityweather.data.network.OpenWeatherDataSource
 import stefanebner.dev.cityweather.model.City
@@ -12,25 +11,29 @@ class CityRepository(
        database: CityDatabase,
        private val dataSourceOpen: OpenWeatherDataSource
 ) {
-
-    private val dao: CityDao
-
-    init {
-        val networkData = dataSourceOpen.getAllCities()
-        dao = database.cityDatabase.cityDao()
-        val observer = Observer<List<City>> { cities -> doAsync { dao.insertAll(cities) } }
-        networkData.observeForever(observer)
-    }
+    private val dao = database.cityDatabase.cityDao()
 
     // id nm lat lon countryCode ->
     // city ID, Name, latitude, longitude, 2letter country code
+    // currently just over 74k cities in the list
     fun startLocalCitySync(inputStream: InputStream) {
-        doAsync {
-            // roughly 74k entries in the text file, restart import if not all are parsed
-            if (getNumberOfCitiesInDatabase() < 74000) {
-                inputStream.bufferedReader().forEachLine { dao.insertCity(City(it.split("\t"))) }
-            }
-        }
+        Observable.just(dao)
+                .subscribeOn(Schedulers.io())
+                .subscribe {
+                    dao -> if (dao.getNumberOfEntries() < 74000) parseLocalFiles(inputStream)
+                }
+    }
+
+    private fun parseLocalFiles(inputStream: InputStream) {
+        parseFile(inputStream)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
+                .subscribe { dao.insertCity(City(it.split("\t"))) }
+    }
+
+    private fun parseFile(inputStream: InputStream) = Observable.create<String> { e ->
+        inputStream.bufferedReader().forEachLine { line -> e.onNext(line) }
+        e.onComplete()
     }
 
     fun getAll() = dao.getAll()
